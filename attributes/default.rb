da2c6e04a1ea1a148fd42f46c4585222c6c6fa1e
vkhatri <vir.khatri@gmail.com>
2014-12-27 12:35:08 +0530

    removing nagios sample config file from cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,11 @@
-
 default['pnp4nagios']['version'] = '0.6.24'
 
 # owner / group
 default['pnp4nagios']['user'] = 'nagios'
 default['pnp4nagios']['group'] = 'nagios'
+default['pnp4nagios']['perms'] = '0644'
+
+default['pnp4nagios']['users']['pnp4nagios'] = '$apr1$WLA2Rsgm$YtldhsVj1PFzrKxIooHNI/' # pnp4nagios:pnp4nagios
 
 default['pnp4nagios']['source_url'] = "http://downloads.sourceforge.net/project/pnp4nagios/PNP-0.6/pnp4nagios-#{node['pnp4nagios']['version']}.tar.gz"
 
@@ -14,36 +16,26 @@ default['pnp4nagios']['conf_dir'] = '/etc/pnp4nagios'
 
 # home dir for various run time directories / files
 default['pnp4nagios']['home_dir'] = '/var/lib/pnp4nagios'
-default['pnp4nagios']['perf_data_dir'] = ::File.join(node['pnp4nagios']['home_dir'], 'perfdata')
-default['pnp4nagios']['spool_dir'] = ::File.join(node['pnp4nagios']['home_dir'], 'spool')
+default['pnp4nagios']['spool_dir'] = '/var/spool/icinga2/perfdata'
 
-# nagios / icinga perf data configuration
-default['pnp4nagios']['service_data_dir'] = ::File.join(node['pnp4nagios']['perf_data_dir'], 'service-perfdata')
-default['pnp4nagios']['service_data_file_template'] = 'DATATYPE::SERVICEPERFDATA\tTIMET::$TIMET$\tHOSTNAME::$HOSTNAME$\tSERVICEDESC::$SERVICEDESC$\tSERVICEPERFDATA::$SERVICEPERFDATA$\tSERVICECHECKCOMMAND::$SERVICECHECKCOMMAND$\tHOSTSTATE::$HOSTSTATE$\tHOSTSTATETYPE::$HOSTSTATETYPE$\tSERVICESTATE::$SERVICESTATE$\tSERVICESTATETYPE::$SERVICESTATETYPE$'
-
-default['pnp4nagios']['host_data_dir'] = ::File.join(node['pnp4nagios']['perf_data_dir'], 'host-perfdata')
-default['pnp4nagios']['host_data_file_template'] = 'DATATYPE::HOSTPERFDATA\tTIMET::$TIMET$\tHOSTNAME::$HOSTNAME$\tHOSTPERFDATA::$HOSTPERFDATA$\tHOSTCHECKCOMMAND::$HOSTCHECKCOMMAND$\tHOSTSTATE::$HOSTSTATE$\tHOSTSTATETYPE::$HOSTSTATETYPE$'
-
-default['pnp4nagios']['rrdbase'] = ::File.join(node['pnp4nagios']['perf_data_dir'], 'service-perfdata')
-
-# npcd configuration
+# npcd.cfg
 default['pnp4nagios']['perfdata_run_cmd_args'] = '--bulk'
 default['pnp4nagios']['npcd_max_threads'] = 5
-default['pnp4nagios']['npcd_max_threads'] = 15
+default['pnp4nagios']['npcd_sleep_time'] = 15
 default['pnp4nagios']['perfdata_file_processing_interval'] = 15
-default['pnp4nagios']['host_data_processing_interval'] = 15
-default['pnp4nagios']['service_data_processing_interval'] = 15
 
-default['pnp4nagios']['auth_file'] = ::File.join(node['pnp4nagios']['conf_dir'], 'htpasswd.users')
+default['pnp4nagios']['auth_file'] = ::File.join(node['pnp4nagios']['conf_dir'], 'pnp4nagios.users')
 
 default['pnp4nagios']['log_dir'] = '/var/log/pnp4nagios'
 default['pnp4nagios']['log_type'] = 'file'
 default['pnp4nagios']['log_level'] = '0'
 default['pnp4nagios']['max_log_file_size'] = '10485760'
 
+default['pnp4nagios']['perf_data_dir'] = '/var/rrdtool/cache'
 default['pnp4nagios']['rrd_listener'] = 'unix://var/rrdtool/rrdcached.sock'
-default['pnp4nagios']['rrd_data_dir'] = '/var/rrdtool/cache'
+default['pnp4nagios']['rrd_stats_dir'] = '/var/rrdtool/stats'
+
+default['pnp4nagios']['use_rrds'] = 0
 default['pnp4nagios']['nagios_base'] = '/nagios/cgi-bin'
 default['pnp4nagios']['multisite_base_url'] = '/nagios'
 default['pnp4nagios']['livestatus_socket'] = 'unix:/usr/local/nagios/var/rw/live'
-
